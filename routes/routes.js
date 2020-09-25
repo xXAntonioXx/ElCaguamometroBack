a0e82202535d3c72f483e54b591bac2074a4d575
xXAntonioXx <jose_antony11@hotmail.com>
2020-09-25 00:01:14 -0700

    se agregaron headers a la ruta principal

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ const beersCatalogue = require('../logic/beerCatalogue.json');
 
 router.get('/showCaguamaPrice', function (req, res) {
     let beers = beersCatalogue.beers;
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.json(beers);
 });
 
